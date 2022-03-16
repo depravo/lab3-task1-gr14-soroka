f03cb8d74494b15ac04aae332d7914881e54bab7
depravo <79765316+depravo@users.noreply.github.com>
2022-03-16 18:47:01 +0000

    Created function maxnumber

--- src/main.c
+++ src/main.c
@@ -1,8 +1,28 @@
-/* hello.c */
 #include <stdio.h>
+#include <math.h>
 
-void main (void)
+int maxnumber(int n) {
+  int s[10] = { 0 };
+  int result = 0;
+  
+  while(n) {
+    s[n % 10]++;
+    n /= 10;
+  }
+  
+  for(int i = 9; i >= 0; i--) {
+    for(int j = 0; j < s[i]; j++) {
+      result *= 10;
+      result += i;
+    }  
+  }
+  
+  return result;
+}
+
+int main (void)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+  
+  
+  return 0;
 }
